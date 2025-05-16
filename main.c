76045d7b03cb7e4b2c3b4d789dc837b6253d315c
Assassinoooo <oioi220419@gmail.com>
2025-05-16 20:55:50 +0700

    Print main menu

--- main.c
+++ main.c
@@ -2,9 +2,20 @@
 
 //batas  include
 
-
+//batas struct
+void printMainMenu(){
+    puts("Pilih Menu: \n");
+    puts("1. Hitung pajak penghasilan\n");
+    puts("2. Hitung pajak penghasilan UMKM\n");
+    puts("3. Rekapitulasi Pajak\n");
+    printf("Pilihan anda: ");
+}
 //batas function lainnya
 int main(){
+    int pilihan;
     puts("Welcome to Simulasi Pajak Indonesia");
+    printMainMenu();
+    scanf("%d",&pilihan);
+    
     return 0;
 }
